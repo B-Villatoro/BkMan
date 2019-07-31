e25e75cf7db46e3a72992353bec7007a73f49b1b
Bryan Villatoro <bvillatoro@ymail.com>
2019-07-31 15:38:10 -0400

    init menu test

---
+++ src/com/company/Menu.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scan = new Scanner(System.in);
+
+    public Menu(){
+        System.out.println("Hello welcome to BookBook!\nHere you will be able to organize your books!");
+    }
+}
